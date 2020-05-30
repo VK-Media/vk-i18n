d665b891842057124e63fe71d4d9f0ef633497c8
Robin Louis van Komen <robin@vkmedia.dk>
2020-05-30 10:06:14 +0200

    export enum from root

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import useTranslation from './hooks/useTranslation'
+import { languages } from './types/i18n.types'
 import { setLanguage } from './utils/language.utils'
 import { initialize } from './utils/setup.utils'
 
-export { useTranslation, setLanguage, initialize }
+export { useTranslation, setLanguage, initialize, languages }
